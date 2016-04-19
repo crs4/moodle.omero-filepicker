1f1032db6582d6a9250191cb79f7b554deb1ba3e
Marco Enrico Piras <kikkomep@crs4.it>
2016-04-19 09:32:48 +0200

    update install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -z ${MOODLE_WWW} ]]; then
+	echo -e "ERROR: MOODLE_WWW not found in your environment !!!"
+	exit -1
+fi
+
+registered=$(grep -o 'MoodleQuickForm_omerofilepicker' ${MOODLE_WWW}/lib/formslib.php | wc -l)
+if grep -o 'MoodleQuickForm_omerofilepicker' ${MOODLE_WWW}/lib/formslib.php ; then
+	echo -e "\n NOTICE: 'omerofilepicker' form already registered."
+else	
+	echo -e "# OmeroFilePicker \nMoodleQuickForm::registerElementType('omerofilepicker', \"\$CFG->libdir/form/omerofilepicker/omerofilepicker.php\", 'MoodleQuickForm_omerofilepicker');" >> ${MOODLE_WWW}/lib/formslib.php
+	echo -e "Registering the omerofilepicker form.... done."
+fi
+
